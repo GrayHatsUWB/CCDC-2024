1b8886fe179c94cd047e8960da2356206998a8ee
ouro-borous <markomoic@gmail.com>
2023-09-23 17:04:09 -0700

    Djicada porting in progress

--- DjicadaComms/djicadacomms/injecttimer/views.py
+++ DjicadaComms/djicadacomms/injecttimer/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+injects = [
+        {
+                'name': 'Create AD User',
+                'type': 'Windows',
+                'time_limit': '120',
+                'resources': 'Desktop01',
+                'services': 'Active Directory',
+                'priority': '3',
+                'notes': 'LEAST PRIVILEGE'
+        },
+        {
+                'name': 'Add cronjob',
+                'type': 'Linux',
+                'time_limit': '180',
+                'resources': 'Desktop04',
+                'services': 'Crontab daemon',
+                'priority': '4',
+                'notes': 'Reset LSMS after adding'
+        }
+]
 
 def home(request):
-        return HttpResponse('<h1>Djicada</h1>')
+        context = {
+                'injects': injects
+        }
+        return render(request, 'injecttimer/inject.html', context)
