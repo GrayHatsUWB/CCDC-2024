6219e284a70e771dd6c52ca321a284f8261e2509
ouro-borous <88122773+ouro-borous@users.noreply.github.com>
2024-01-03 18:39:21 -0800

    Update InitialLockdown.sh

--- Linux/InitialLockdown.sh
+++ Linux/InitialLockdown.sh
@@ -1,1 +1,20 @@
+#RUN AS SUDO
+#PASSWORDS
+#Change password to something secure
+#Install pampwquality
+
+#USERS
+#Verify password file
+#Verify groups file
+#Verify sudo file
+#Verify home folder ownership 
+
+#BINARIES + SCRIPTS
+#Check Parse PATH into multiple directories
+#Check if any of those directories have a strange file type?
+
+#SYSTEM
+#Secure kernel
+#Disable forwarding, IPv6, 
+#Disable root login and harden if sshd exists
 
